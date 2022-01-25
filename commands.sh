3ac5f915ebab2995d36b85d89c6321fa213ef0f2
Andrew Marsh <marshandrewg@gmail.com>
2022-01-24 23:31:03 -0500

    Added initial shell script to run after container creation

--- commands.sh
+++ commands.sh
@@ -1,5 +1,6 @@
+cd /code/fridge_bot
 nohup go run . </dev/null &>/dev/null &
 export BOT_TOKEN $TOKEN
 FLASK_APP=image_rest_api.py FLASK_ENV=development flask run &
-d /code/fridge_bot/ \
+cd /code/fridge_bot/ \
 	&& go run . 
